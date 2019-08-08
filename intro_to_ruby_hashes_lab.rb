563e4ec5428e57ea0baf05ad2126886f46449ac9
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 17:33:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,31 +6,53 @@ end
 
 def monopoly_with_second_tier
   monopoly_board ={
-		:first_line => {
+		:main_line => {
 
+			},
 		},
-		:second_line => {
+		:stream_line => {
 
-		},
-		:third_line => {
-
-		},
-		:fourth_line => {
-
-		},
 	}
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	monopoly_board ={
+		:main_line => {
+			"Red" => {
+				
+			},
+			"Yellow" => {
+				
+			},
+		},
+		:stream_line => {
+			"Blue" => {
+				
+			},
+			"Green" => {
+				
+			},
+		},
+	}
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	monopoly_board ={
+		:main_line => {
+			"Red" => {
+				
+			},
+			"Yellow" => {
+				
+			},
+		},
+		:stream_line => {
+			"Blue" => {
+				
+			},
+			"Green" => {
+				
+			},
+		},
+	}
 end
