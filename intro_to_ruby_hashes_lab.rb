1d354f90d76b668fbaefb4e08ae9408bbf6acfc0
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 17:38:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,18 +40,22 @@ def monopoly_with_fourth_tier
 	monopoly_board ={
 		:main_line => {
 			"Red" => {
-
+				:main_street => "Illinois Ave.",
+				:side_street => "Kentucky Ave."
 			},
 			"Yellow" => {
-
+				:main_street => "Marvin Gardens"
+				:side_street => "Atlantic Ave."
 			},
 		},
 		:stream_line => {
 			"Blue" => {
-
+				:main_street => "Boardwalk"
+				:side_street => "Park Place"
 			},
 			"Green" => {
-
+				:main_street => "Pennsylvania Ave."
+				:side_street => "Pacific Ave."
 			},
 		},
 	}
